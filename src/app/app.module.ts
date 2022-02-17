f8a7337ff27eb3b2ad41a3a92c821578c3a33b8d
blackCaleb <bblackcaleb@gmail.com>
2022-02-17 14:22:03 +0300

    reupdated

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { DatepipePipe } from './datepipe.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfilePageComponent,
+    DatepipePipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
